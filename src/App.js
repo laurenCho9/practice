1ddbce673f8a0b23cc25e74e271eddaa08001cee
laurenCho9 <lauren.choux@gmail.com>
2023-02-18 05:47:44 +0900

    투두리스트 삭제버튼까지 완료(모듈 제거 후 재업로드)

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
+import "./App.css";
+import { useState } from "react";
+import Todos from "./component/Todos";
+import TodoList from "./component/TodoList";
 
+function App() {
+  return (
+    <>
+      <Todos />
+
+      {/* <TodoList style={{ color: "red" }} /> */}
+    </>
+  );
+}
+
+export default App;
