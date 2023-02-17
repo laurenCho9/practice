d525b7c09502a2f3824d1b049f77ab0603644c1b
laurenCho9 <lauren.choux@gmail.com>
2023-02-18 02:24:23 +0900

    연결

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  return (
+    <div>
+      <input type="text"></input>
+      <button>Upload</button>
+    </div>
+  );
+}
+
+export default App;
